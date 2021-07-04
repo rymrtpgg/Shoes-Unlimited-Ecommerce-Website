4890fe2c92de0b274547886e059a766da31abf18
raymart <tramyarnanulatgap@gmail.com>
2021-07-04 15:48:05 +0800

    add readwishlit()

--- wishlistFunction.php
+++ wishlistFunction.php
@@ -2,41 +2,46 @@
 
 class wishListCrud {
 
-
 	// Read wish list function start
 	function readWishList() {
-
 	  $xml = new DomDocument;
 	  $xml->load('wishlist.xml');
 	  $x = $xml->getElementsByTagName('shoes')->item(0);
 	  $all_prod = $x->getElementsByTagName('shoe');
 	  $tf = 0;
-	      
+
 	  foreach ($all_prod as $each_pro) {
 
 	    $id    = $each_pro->getElementsByTagName('id')->item(0)->nodeValue;
 	    $image = $each_pro->getElementsByTagName('image')->item(0)->nodeValue;
 	    $name  = $each_pro->getElementsByTagName('shoeName')->item(0)->nodeValue;
-	    $price = $each_pro->getElementsByTagName('price')->item(0)->nodeValue;
+	    $price_Format = $each_pro->getElementsByTagName('price')->item(0)->nodeValue;
 	    $color = $each_pro->getElementsByTagName('color')->item(0)->nodeValue;
+		  $price = number_format($price_Format, 2);    
 
-	    echo "<div class='container'>";
-	    echo "<div class='col'>";
-	    echo "<div class='col-sm-4'>";
+	    echo "<div class='col-sm-4 cardBx'> ";
+	    echo "<form class = 'form' method='POST' action='cartMan.php'>";
 	    echo "<div class='card'>";
-	    echo "<img src='". $image ."'>";
-	    echo "<h1>React-Infinity-Run</h1>";
-	    echo "<p class='price'>".$price."</p>";
+	    echo "<img name='image' src=' " . $image . "' alt='nike' width='100%' height='330'>";
+	    echo "<p><h1 name='prodName' value ='React-Infinity-Run'> ". $name . $id . " </h1></p>";
+	    echo "<p name='price' > " . $price . "</p>";
 	    echo "<p>Color: ". $color ."</p>";
-	    echo "<p><button>Add to Cart</button></p>";
-	    echo "<p><button>Remove</button></p>";
-	    echo "</div> "; 
+	    echo "<p><button type='submit' class='cartBtn' id='cartBtn' name='cartBtn'>Move to Cart</button></p>";
+	    echo "<p><button type='submit' class='wishBtn' id='wishBtn' name='wishBtn'>Add to WishList</button></p>";
+	    echo "<input type='hidden' name='id' value='".$id."'>";
+	    echo "<input type='hidden' name='image' value='".$image."'>";
+	    echo "<input type='hidden' name='prodName' value='".$name."'>";
+	    echo "<input type='hidden' name='price' value='".$price."'>";
+	    echo "<input type='hidden' name='color' value='".$color."'>";
 	    echo "</div>";
-	    echo "</div>";
-	    echo "</div>";
-
+	    echo "</form>";
+	    echo "</div>  ";	
 		}
 	}// Read wish list function End
+
+
+
+
 
 
 
